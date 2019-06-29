6754082388fc256c7bd2e4877060c917b76b30a5
aadi <aadiyaara@gmail.com>
2019-06-29 23:43:27 +0530

    prints thr number of people connected

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var io = socket(server)
 io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id)
-
+    console.log('Connections: ', io.sockets.clients().length)
     // Handle chat event
     socket.on('chat', function(data){
         // console.log(data)
