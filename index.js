791c188baf2e11d4494b233857b4747454b8d8ae
aadi <aadiyaara@gmail.com>
2019-07-01 22:21:40 +0530

    State flow checks

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ io.on('connection', (socket) => {
 
     socket.on('state', function(data){
         socket.broadcast.to(socket.request.url.split('userId=')[1].split('&EIO=')[0]).emit('chat', data)
+        console.log('State data exchanged')
     })
 
 })
